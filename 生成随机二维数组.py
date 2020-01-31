9354af951b91729381aabda1359b1d6f7f4a9bce
Nordenbox <nordenbox@gmail.com>
2020-01-31 21:46:41 +0800

    添加了参数输入

--- 生成随机二维数组.py
+++ 生成随机二维数组.py
@@ -2,12 +2,15 @@ import numpy as np
 import random
 
 
-b = []
+Origin_List = []
+Dimentions = int(input('how many dimentions di you want: '))
+range_scale = int(input('how large the random numbers: '))
 
-for i in range(1, 5):
-    b.append([random.randint(0, 10) for i in range(1, 5)]) 
+for i in range(1, Dimentions+1):
+    Origin_List.append([random.randint(0, range_scale+1) for i in range(1, Dimentions+1)])
     #    这个地方用了表达式，因为一开始使用的嵌套的 for 循环让我迷失了。
 
-print(b)
-new_four_dimensions = np.array(b)
+print(Origin_List)
+new_four_dimensions = np.array(Origin_List)
 print(new_four_dimensions)
+
