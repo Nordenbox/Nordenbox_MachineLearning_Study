af06de986d678e1667add45fe9e2b4a52093334a
Nordenbox <nordenbox@gmail.com>
2020-02-02 13:08:43 +0800

    Update 生成随机二维数组.py
    添加了循环语句

--- 生成随机二维数组.py
+++ 生成随机二维数组.py
@@ -9,6 +9,10 @@ range_scale = int(input('how large the random numbers: '))
 for i in range(1, Dimentions+1):
     Origin_List.append([random.randint(0, range_scale+1) for i in range(1, Dimentions+1)])
     #    这个地方用了表达式，因为一开始使用的嵌套的 for 循环让我迷失了。
+# for i in range(1, 5):
+#     a.append(random.randint(0, 10))
+#     b.append(a)
+#     其实不用两层循环，一层就可以了。因为 append 函数返回的还是一个列表，包含了原来的列表。
 
 print(Origin_List)
 new_four_dimensions = np.array(Origin_List)
