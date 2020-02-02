441ca6745b399b9efe4a976b698404acd0baadc2
Nordenbox <nordenbox@gmail.com>
2020-02-02 17:02:01 +0800

    增加类与方法
    改用类和对象的方法来写，增加了多项输出功能。

--- 生成随机n维数组.py
+++ 生成随机n维数组.py
@@ -2,23 +2,37 @@ import numpy as np
 import random
 
 
-Origin_List = []
-Dimentions_rows = int(input('how many rows do you want: '))
-Dimentions_columns = int(input('how many columns do you want: '))
+Dimentions_rows = int(input('how many rows/matrix do you want: '))
+Dimentions_columns = int(input('how many columns/matrix do you want: '))
 random_scale = int(input('how large the random numbers: '))
+numbers_matrix = int(input('how many matrix do you want: '))
 
-for i in range(1, Dimentions_rows+1):
-    Origin_List.append([random.randint(0, random_scale+1) for i in range(1, Dimentions_columns+1)])
-    #    这个地方用了表达式，因为一开始使用的嵌套的 for 循环让我迷失了。
 
-# for i in range(1, 5):
-#     a.append(random.randint(0, 10))
-#     b.append(a)
-#     其实不用两层循环，一层就可以了。因为 append 函数返回的还是一个列表，包含了原来的列表。
+class MakeRandomList:
+    def __init__(self):
+        self.Dimentions_rows = Dimentions_rows
+        self.Dimentions_columns = Dimentions_columns
+        self.random_scale = random_scale
 
-#print(Origin_List)
-new_four_dimensions = np.array(Origin_List)
-print(new_four_dimensions)
-print('this matrix\'s rows and columbs is ', new_four_dimensions.shape)
-print(new_four_dimensions.dtype)
+    def creating(self, Dimentions_rows, Dimentions_columns, random_scale):
+        Origin_List = []
+        for i in range(1, Dimentions_rows + 1):
+            Origin_List.append([random.randint(0, random_scale) for i in range(1, Dimentions_columns + 1)])
+        return Origin_List
+        #    这个地方用了表达式，因为一开始使用的嵌套的 for 循环让我迷失了。
+
+    # for i in range(1, 5):
+    #     a.append(random.randint(0, 10))
+    #     b.append(a)
+    #     其实不用两层循环，一层就可以了。因为 append 函数返回的还是一个列表，包含了原来的列表。
+
+
+# print(Origin_List)
+creat_list = MakeRandomList()
+
+for s in range(numbers_matrix):
+    i = creat_list.creating(Dimentions_rows, Dimentions_columns, random_scale)
+    print('No.',s+1,'Matrix is:\n', np.array(i))
+    print('*******')
+
 
