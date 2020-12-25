7a54d72fee69e6cedecbf586791c0853e1402fd2
Nordenbox <nordenbox@gmail.com>
2020-12-26 07:48:59 +0800

    update

--- .history/Playground_20201226073803.py
+++ .history/Playground_20201226073803.py
@@ -1,7 +1,7 @@
 
-number = [int(x) for x in range(1000)]
-#print(number)
-game_number = input('game number is ')
+number = [int(x) for x in range(1, 1000)]
+game_number = int(input('不许说出来的数字是： '))
+
 for i in number:
     if i % game_number == 0 or i % 10 == game_number:
         print(i)
