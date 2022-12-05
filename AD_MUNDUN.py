3926edc4329e6d72fe4605a146cd45912ea29b1e
Nordenbox <47683980+Nordenbox@users.noreply.github.com>
2022-12-05 06:26:21 -0500

    update

---
+++ AD_MUNDUN.py
@@ -0,0 +1,12 @@
+def sieve(n):
+    x=[1]*n
+    x[1]=0
+    
+    for i in range(2, n/2):
+        j = 2*i
+        while j < n:
+            x[j] =0
+            j=j+1
+    return x
+
+print(sieve(100))
